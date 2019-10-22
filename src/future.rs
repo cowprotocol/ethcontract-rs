6a401762b0d451b98afef15705618512108d2572
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-10-22 18:59:42 +0200

    slightly more self-explanitory MaybeReady implementation

--- src/future.rs
+++ src/future.rs
@@ -1,37 +1,33 @@
-use futures::future::MaybeDone;
+use futures::future::{self, Ready, Either};
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 /// Utility type for a future that might be ready. Similar to `MaybeDone` but
 /// not fused.
-pub struct MaybeReady<F: Future + Unpin>(MaybeDone<F>);
+pub struct MaybeReady<F: Future>(Either<Ready<F::Output>, F>);
 
 impl<F: Future + Unpin> MaybeReady<F> {
     /// Get a pinned reference to the fused inner `MaybeDone` value.
-    fn inner(self: Pin<&mut Self>) -> Pin<&mut MaybeDone<F>> {
+    fn inner(self: Pin<&mut Self>) -> Pin<&mut Either<Ready<F::Output>, F>> {
         Pin::new(&mut self.get_mut().0)
     }
 
     /// Create a new `MaybeReady` with an immediate value.
     pub fn ready(value: F::Output) -> MaybeReady<F> {
-        MaybeReady(MaybeDone::Done(value))
+        MaybeReady(Either::Left(future::ready(value)))
     }
 
     /// Create a new `MaybeReady` with a deferred value.
     pub fn future(fut: F) -> MaybeReady<F> {
-        MaybeReady(MaybeDone::Future(fut))
+        MaybeReady(Either::Right(fut))
     }
 }
 
 impl<F: Future + Unpin> Future for MaybeReady<F> {
     type Output = F::Output;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
-        self.as_mut().inner().poll(cx).map(|_| {
-            self.inner()
-                .take_output()
-                .expect("should only be called once")
-        })
+    fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        self.inner().poll(cx)
     }
 }
