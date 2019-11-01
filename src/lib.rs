9fd4f359c110928774ad968620d0e27641f07651
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-01 17:02:00 +0100

    added cargo feature 'example' gating building of example contracts

--- src/lib.rs
+++ src/lib.rs
@@ -36,13 +36,19 @@ mod test {
     pub mod transport;
 }
 
+#[cfg(feature = "example")]
 #[allow(missing_docs)]
 pub mod example {
     //! An example of a derived contract for documentation purposes in roder to
     //! illustrate what the generated API. This module should not be used and is
     //! should only be included when generating documentation.
 
-    // use crate as ethcontract;
-    // ethcontract::contract!("contracts/build/contracts/DocumentedContract.json");
-    // ethcontract::contract!("contracts/build/contracts/LinkedContract.json");
+    crate::contract!(
+        "contracts/build/contracts/DocumentedContract.json",
+        crate = crate
+    );
+    crate::contract!(
+        "contracts/build/contracts/LinkedContract.json",
+        crate = crate
+    );
 }
