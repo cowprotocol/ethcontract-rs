9ca4be35c2cc9ed4859a168b4d452d2dfe21be4a
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-08 08:40:40 +0100

    with async/await stablized we can have the example always compile

--- src/test/prelude.rs
+++ src/test/prelude.rs
@@ -1,7 +1,21 @@
 //! Prelude module with common types used for unit tests.
 
-pub use crate::future::FutureWaitExt;
 pub use crate::test::macros::*;
 pub use crate::test::transport::TestTransport;
 pub use serde_json::json;
+use std::future::Future;
 pub use web3::api::Web3;
+
+/// Temporary solution to work around the issue that async tests are not stable
+/// and the extra boiler plate of setting up an executor is inconvenient.
+/// TODO(nlordell): remove once async tests stablalize
+pub trait FutureWaitExt: Future {
+    /// Block thread on a future completing.
+    fn wait(self) -> Self::Output;
+}
+
+impl<F: Future + Sized> FutureWaitExt for F {
+    fn wait(self) -> Self::Output {
+        futures::executor::block_on(self)
+    }
+}
