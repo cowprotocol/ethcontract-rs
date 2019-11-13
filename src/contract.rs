99a71aa463a219bbf402413c498609dec7c136e4
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-13 17:36:00 +0100

    cargo clippy
    Co-Authored-By: Benjamin Smith <bh2smith@users.noreply.github.com>

--- src/contract.rs
+++ src/contract.rs
@@ -7,7 +7,7 @@ mod method;
 
 use crate::errors::{DeployError, LinkError};
 use crate::truffle::{Abi, Artifact, Bytecode};
-use ethabi::{Result as AbiResult};
+use ethabi::Result as AbiResult;
 use web3::api::Web3;
 use web3::contract::tokens::{Detokenize, Tokenize};
 use web3::types::{Address, Bytes};
