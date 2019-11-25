da17deab3c0c72786f2cf2c8fdf657c91de1b143
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-25 11:17:13 +0100

    cargo fmt

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -6,10 +6,10 @@
 extern crate proc_macro;
 
 use ethcontract_generate::Builder;
-use proc_macro::{TokenStream};
-use syn::{parse_macro_input, Token, Ident, LitStr, Error as SynError};
+use proc_macro::TokenStream;
 use syn::ext::IdentExt;
 use syn::parse::{Error as ParseError, Parse, ParseStream, Result as ParseResult};
+use syn::{parse_macro_input, Error as SynError, Ident, LitStr, Token};
 
 /// Proc macro to generate type-safe bindings to a contract. See
 /// [`ethcontract`](ethcontract) module level documentation for more information.
@@ -17,7 +17,8 @@ use syn::parse::{Error as ParseError, Parse, ParseStream, Result as ParseResult}
 pub fn contract(input: proc_macro::TokenStream) -> TokenStream {
     let args = parse_macro_input!(input as ContractArgs);
     let artifact_span = args.artifact_path.span();
-    args.builder.generate()
+    args.builder
+        .generate()
         .map(|bindings| bindings.into_tokens())
         .unwrap_or_else(|e| SynError::new(artifact_span, e.to_string()).to_compile_error())
         .into()
@@ -29,11 +30,11 @@ struct ContractArgs {
     builder: Builder,
 }
 
-    // TODO(nlordell): Due to limitation with the proc-macro Span API, we can't
-    //   currently get a path the the file where we were called from; therefore,
-    //   the path will always be rooted on the cargo manifest directory.
-    //   Eventually we can use the `Span::source_file` API to have a better
-    //   experience.
+// TODO(nlordell): Due to limitation with the proc-macro Span API, we can't
+//   currently get a path the the file where we were called from; therefore,
+//   the path will always be rooted on the cargo manifest directory.
+//   Eventually we can use the `Span::source_file` API to have a better
+//   experience.
 
 impl Parse for ContractArgs {
     fn parse(input: ParseStream) -> ParseResult<Self> {
@@ -55,7 +56,7 @@ impl Parse for ContractArgs {
                     let ident = input.call(Ident::parse_any)?;
                     let name = format!("{}", ident.unraw());
                     builder = builder.with_runtime_crate_name(&name);
-                },
+                }
                 _ => {
                     return Err(ParseError::new(
                         param.span(),
@@ -65,7 +66,9 @@ impl Parse for ContractArgs {
             }
         }
 
-        Ok(ContractArgs { artifact_path, builder })
+        Ok(ContractArgs {
+            artifact_path,
+            builder,
+        })
     }
 }
-
