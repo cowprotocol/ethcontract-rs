0ea891f1f3265676f9d3759dca272b8c0802fb37
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-28 12:46:49 +0100

    added truffle wallet and verify plugin

--- examples/truffle/truffle-config.js
+++ examples/truffle/truffle-config.js
@@ -1,12 +1,30 @@
-//! Simple truffle configuration with configured develop network for testing
-//! our rust examples.
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const {
+  PK,
+  INFURA_PROJECT_ID,
+  ETHERSCAN_API_KEY
+} = process.env;
 
 module.exports = {
-  networks: { },
+  networks: {
+    rinkeby: {
+      provider = () =>
+        new HDWalletProvider(PK, `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`),
+    },
+  },
 
   mocha: { },
 
   compilers: {
     solc: { },
+  },
+
+  plugins: [
+    "truffle-plugin-verify",
+  ],
+
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
 };
