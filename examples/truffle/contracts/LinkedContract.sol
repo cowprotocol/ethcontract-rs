dcb217919ce4896bba4665a9d4a63043ce38ba81
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-11-29 10:34:48 +0100

    added linked example and fixed a small but in generated linking code

--- examples/truffle/contracts/LinkedContract.sol
+++ examples/truffle/contracts/LinkedContract.sol
@@ -4,7 +4,7 @@ import "./SimpleLibrary.sol";
 
 contract LinkedContract {
   using SimpleLibrary for uint256;
-  
+
   function callAnswer() public pure returns (uint256) {
     return uint256(0).answer();
   }
