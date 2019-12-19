2d87127d241cf452032c33e587b61d8acf05be61
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2019-12-19 15:52:10 +0100

    [Easy] Fixed Some Copy-Pasta Errors In `MethodBuilder` (#84)
    And added some unit tests to method builder to confirm transaction is as expected.
    
    ### Test Plan
    
    CI
    
    ### Commit History
    
    * fixed some typos and added some unit tests
    
    * better view method tests

--- src/contract/method.rs
+++ src/contract/method.rs
@@ -83,21 +83,21 @@ impl<T: Transport, R> MethodBuilder<T, R> {
     /// Specify the gas price to use, if not specified then the estimated gas
     /// price will be used.
     pub fn gas_price(mut self, value: U256) -> MethodBuilder<T, R> {
-        self.tx = self.tx.gas(value);
+        self.tx = self.tx.gas_price(value);
         self
     }
 
     /// Specify what how much ETH to transfer with the transaction, if not
     /// specified then no ETH will be sent.
     pub fn value(mut self, value: U256) -> MethodBuilder<T, R> {
-        self.tx = self.tx.gas(value);
+        self.tx = self.tx.value(value);
         self
     }
 
     /// Specify the nonce for the transation, if not specified will use the
     /// current transaction count for the signing account.
     pub fn nonce(mut self, value: U256) -> MethodBuilder<T, R> {
-        self.tx = self.tx.gas(value);
+        self.tx = self.tx.nonce(value);
         self
     }
 
@@ -181,14 +181,14 @@ impl<T: Transport, R: Detokenize> ViewMethodBuilder<T, R> {
     /// Specify the gas price to use, if not specified then the estimated gas
     /// price will be used.
     pub fn gas_price(mut self, value: U256) -> ViewMethodBuilder<T, R> {
-        self.m = self.m.gas(value);
+        self.m = self.m.gas_price(value);
         self
     }
 
     /// Specify what how much ETH to transfer with the transaction, if not
     /// specified then no ETH will be sent.
     pub fn value(mut self, value: U256) -> ViewMethodBuilder<T, R> {
-        self.m = self.m.gas(value);
+        self.m = self.m.value(value);
         self
     }
 
@@ -247,5 +247,150 @@ impl<T: Transport, R: Detokenize> Future for CallFuture<T, R> {
         self.inner()
             .poll(cx)
             .map(|result| result.map_err(ExecutionError::from))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::prelude::*;
+    use crate::truffle::abi::{Param, ParamType};
+
+    fn test_abi_function() -> (Function, Bytes) {
+        let function = Function {
+            name: "test".to_owned(),
+            inputs: Vec::new(),
+            outputs: vec![Param {
+                name: "".to_owned(),
+                kind: ParamType::Uint(256),
+            }],
+            constant: false,
+        };
+        let data = function
+            .encode_input(&[])
+            .expect("error encoding empty input");
+
+        (function, Bytes(data))
+    }
+
+    #[test]
+    fn method_tx_options() {
+        let transport = TestTransport::new();
+        let web3 = Web3::new(transport.clone());
+
+        let address = addr!("0x0123456789012345678901234567890123456789");
+        let from = addr!("0x9876543210987654321098765432109876543210");
+        let (function, data) = test_abi_function();
+        let tx = MethodBuilder::<_, U256>::new(web3, function, address, data.clone())
+            .from(Account::Local(from, None))
+            .gas(1.into())
+            .gas_price(2.into())
+            .value(28.into())
+            .nonce(42.into())
+            .into_inner();
+
+        assert_eq!(tx.from.map(|a| a.address()), Some(from));
+        assert_eq!(tx.to, Some(address));
+        assert_eq!(tx.gas, Some(1.into()));
+        assert_eq!(tx.gas_price, Some(2.into()));
+        assert_eq!(tx.value, Some(28.into()));
+        assert_eq!(tx.data, Some(data));
+        assert_eq!(tx.nonce, Some(42.into()));
+        transport.assert_no_more_requests();
+    }
+
+    #[test]
+    fn view_method_call() {
+        let mut transport = TestTransport::new();
+        let web3 = Web3::new(transport.clone());
+
+        let address = addr!("0x0123456789012345678901234567890123456789");
+        let from = addr!("0x9876543210987654321098765432109876543210");
+        let (function, data) = test_abi_function();
+        let tx = ViewMethodBuilder::<_, U256>::from_method(MethodBuilder::new(
+            web3,
+            function,
+            address,
+            data.clone(),
+        ))
+        .from(from)
+        .gas(1.into())
+        .gas_price(2.into())
+        .value(28.into())
+        .block(BlockNumber::Number(100));
+
+        transport.add_response(json!(
+            "0x000000000000000000000000000000000000000000000000000000000000002a"
+        )); // call response
+        let result = tx.call().wait().expect("call error");
+
+        assert_eq!(result, 42.into());
+        transport.assert_request(
+            "eth_call",
+            &[
+                json!({
+                    "from": from,
+                    "to": address,
+                    "gas": "0x1",
+                    "gasPrice": "0x2",
+                    "value": "0x1c",
+                    "data": data,
+                }),
+                json!("0x64"),
+            ],
+        );
+        transport.assert_no_more_requests();
+    }
+
+    #[test]
+    fn method_to_view_method_preserves_options() {
+        let mut transport = TestTransport::new();
+        let web3 = Web3::new(transport.clone());
+
+        let address = addr!("0x0123456789012345678901234567890123456789");
+        let (function, data) = test_abi_function();
+        let tx = MethodBuilder::<_, U256>::new(web3, function, address, data.clone())
+            .gas(42.into())
+            .view();
+
+        transport.add_response(json!(
+            "0x0000000000000000000000000000000000000000000000000000000000000000"
+        ));
+        tx.call().wait().expect("call error");
+
+        transport.assert_request(
+            "eth_call",
+            &[
+                json!({
+                    "to": address,
+                    "gas": "0x2a",
+                    "data": data,
+                }),
+                json!("latest"),
+            ],
+        );
+        transport.assert_no_more_requests();
+    }
+
+    #[test]
+    fn method_defaults_are_applied() {
+        let transport = TestTransport::new();
+        let web3 = Web3::new(transport.clone());
+
+        let from = addr!("0x9876543210987654321098765432109876543210");
+        let address = addr!("0x0123456789012345678901234567890123456789");
+        let (function, data) = test_abi_function();
+        let tx = MethodBuilder::<_, U256>::new(web3, function, address, data)
+            .with_defaults(&MethodDefaults {
+                from: Some(Account::Local(from, None)),
+                gas: Some(1.into()),
+                gas_price: Some(2.into()),
+            })
+            .into_inner();
+
+        assert_eq!(tx.from.map(|a| a.address()), Some(from));
+        assert_eq!(tx.gas, Some(1.into()));
+        assert_eq!(tx.gas_price, Some(2.into()));
+        transport.assert_no_more_requests();
     }
 }
