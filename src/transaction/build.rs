3b77b7289b61e39773ec6b0d1c4950e370251124
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-02-10 15:23:19 +0100

    Fix gas estimation for contract deploy transactions

--- src/transaction/build.rs
+++ src/transaction/build.rs
@@ -8,7 +8,7 @@ use crate::errors::ExecutionError;
 use crate::future::{CompatCallFuture, MaybeReady};
 use crate::secret::{Password, PrivateKey};
 use crate::sign::TransactionData;
-use crate::transaction::estimate_gas::EstimateGasFuture;
+use crate::transaction::estimate_gas::{EstimateGasFuture, EstimateGasRequest};
 use crate::transaction::gas_price::{
     GasPrice, ResolveGasPriceFuture, ResolveTransactionRequestGasPriceFuture,
 };
@@ -23,8 +23,7 @@ use std::task::{Context, Poll};
 use web3::api::Web3;
 use web3::helpers::CallFuture;
 use web3::types::{
-    Address, Bytes, CallRequest, RawTransaction, TransactionCondition, TransactionRequest, U256,
-    U64,
+    Address, Bytes, RawTransaction, TransactionCondition, TransactionRequest, U256, U64,
 };
 use web3::Transport;
 
@@ -359,11 +358,11 @@ impl<T: Transport> BuildOfflineSignedTransactionFuture<T> {
                 options.gas,
                 EstimateGasFuture::from_request(
                     eth.clone(),
-                    CallRequest {
+                    EstimateGasRequest {
                         from: Some(from),
                         to: to.unwrap_or_else(Address::zero),
                         gas: None,
-                        gas_price: None,
+                        gas_price: gas_price.value(),
                         value: options.value,
                         data: options.data.clone(),
                     }
