5bb3a20976fdd6efe6408e93447502a7684ed7c7
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-02-19 20:46:35 +0100

    support method names with keyword identifiers (#204)
    This PR fixes a small issue where method names that were reserved Rust keywords were causing compile errors.
    
    ### Test Plan
    
    CI

--- generate/src/util.rs
+++ generate/src/util.rs
@@ -3,10 +3,19 @@ use curl::easy::Easy;
 use ethcontract_common::Address;
 use proc_macro2::{Ident, Literal, Span, TokenStream};
 use quote::quote;
+use syn::Ident as SynIdent;
 
 /// Expands a identifier string into an token.
 pub fn ident(name: &str) -> Ident {
     Ident::new(name, Span::call_site())
+}
+
+/// Expands an identifier string into a token and appending `_` if the
+/// identifier is for a reserved keyword.
+///
+/// Parsing keywords like `self` can fail, in this case we add an underscore.
+pub fn safe_ident(name: &str) -> Ident {
+    syn::parse_str::<SynIdent>(&name).unwrap_or_else(|_| ident(&format!("{}_", name)))
 }
 
 /// Expands a doc string into an attribute token stream.
