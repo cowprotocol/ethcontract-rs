0278cdcb040066277c345818dc181060e0d5de68
Nicholas Rodrigues Lordello <nlordell@gmail.com>
2020-03-31 13:55:58 +0200

    don't format the ethcontract using statement as it can lead to lint errors

--- generate/src/contract.rs
+++ generate/src/contract.rs
@@ -154,6 +154,7 @@ fn expand_contract(cx: &Context) -> Result<TokenStream> {
     Ok(quote! {
         #[allow(dead_code)]
         #vis mod #contract_mod {
+            #[rustfmt::skip]
             use #runtime_crate as ethcontract;
 
             #common
