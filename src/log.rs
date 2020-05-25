429757038223a3cf79c98fc23c4dc36f90cc922d
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-05-25 11:02:19 +0000

    [Events] Add `limit` option back (#315)
    The limit option was removed during a refactor, this PR adds it back.
    
    ### Test Plan
    
    New unit test to make sure it is properly being added to the logs query.

--- src/log.rs
+++ src/log.rs
@@ -40,19 +40,15 @@ pub struct LogFilterBuilder<T: Transport> {
     pub address: Vec<Address>,
     /// Topic filters used for filtering logs based on indexed topics.
     pub topics: TopicFilter,
+    /// Limit the number of events that can be retrieved by this filter.
+    ///
+    /// Note that this is option is non-standard.
+    pub limit: Option<usize>,
 
     /// The page size in blocks to use when doing a paginated query on past
     /// logs. This provides no guarantee in how many logs will be returned per
     /// page, but used to limit the block range for the query.
     pub block_page_size: Option<NonZeroU64>,
-    /// The number of blocks to confirm the logs with. This is the number of
-    /// blocks mined on top of the block where the log was emitted for it to be
-    /// considered confirmed.
-    ///
-    /// Live log streaming keeps track of events in the last `confirmations`
-    /// blocks so that reorgs can be detected and a `Removed` logs can be
-    /// emitted by the log stream. Omit this property for a sensible default.
-    pub confirmations: Option<usize>,
     /// The polling interval for querying the node for more logs.
     pub poll_interval: Option<Duration>,
 }
@@ -66,8 +62,8 @@ impl<T: Transport> LogFilterBuilder<T> {
             to_block: None,
             address: Vec::new(),
             topics: TopicFilter::default(),
+            limit: None,
             block_page_size: None,
-            confirmations: None,
             poll_interval: None,
         }
     }
@@ -124,6 +120,14 @@ impl<T: Transport> LogFilterBuilder<T> {
         self
     }
 
+    /// Limit the number of events that can be retrieved by this filter.
+    ///
+    /// Note that this parameter is non-standard.
+    pub fn limit(mut self, value: usize) -> Self {
+        self.limit = Some(value);
+        self
+    }
+
     /// The page size in blocks to use when doing a paginated query on past
     /// events.
     ///
@@ -132,13 +136,6 @@ impl<T: Transport> LogFilterBuilder<T> {
     /// Panics if a block page size of 0 is specified.
     pub fn block_page_size(mut self, value: u64) -> Self {
         self.block_page_size = Some(NonZeroU64::new(value).expect("block page size cannot be 0"));
-        self
-    }
-
-    /// The number of blocks mined after a log has been emitted until it is
-    /// considered confirmed and can no longer be reorg-ed.
-    pub fn confirmations(mut self, value: usize) -> Self {
-        self.confirmations = Some(value);
         self
     }
 
@@ -152,17 +149,20 @@ impl<T: Transport> LogFilterBuilder<T> {
     /// Returns a web3 filter builder needed for querying and streaming logs.
     pub fn into_filter(self) -> FilterBuilder {
         let mut filter = FilterBuilder::default();
+        if let Some(from_block) = self.from_block {
+            filter = filter.from_block(from_block);
+        }
+        if let Some(to_block) = self.to_block {
+            filter = filter.to_block(to_block);
+        }
         if !self.address.is_empty() {
             filter = filter.address(self.address);
         }
         if self.topics != TopicFilter::default() {
             filter = filter.topic_filter(self.topics)
         }
-        if let Some(from_block) = self.from_block {
-            filter = filter.from_block(from_block);
-        }
-        if let Some(to_block) = self.to_block {
-            filter = filter.to_block(to_block);
+        if let Some(limit) = self.limit {
+            filter = filter.limit(limit)
         }
 
         filter
@@ -181,7 +181,11 @@ impl<T: Transport> LogFilterBuilder<T> {
 
     /// Returns a stream that resolves into a page of logs matching the filter
     /// builder's parameters.
-    pub fn past_logs_pages(self) -> impl Stream<Item = Result<Vec<Log>, ExecutionError>> {
+    pub fn past_logs_pages(mut self) -> impl Stream<Item = Result<Vec<Log>, ExecutionError>> {
+        // NOTE: Ignore the `limit` option when doing paginated queries as it
+        //   can interfere.
+        self.limit = None;
+
         stream::try_unfold(PastLogsStream::Init(self), PastLogsStream::next)
             .try_filter(|logs| future::ready(!logs.is_empty()))
     }
@@ -375,26 +379,47 @@ mod tests {
     }
 
     #[test]
-    fn log_stream_next_log() {
+    fn past_logs_options() {
         let mut transport = TestTransport::new();
         let web3 = Web3::new(transport.clone());
 
-        // filter created
-        transport.add_response(json!("0xf0"));
-        // get logs filter
+        let address = Address::repeat_byte(0x42);
+        let topics = (0..=3).map(H256::repeat_byte).collect::<Vec<_>>();
+
+        // get logs
         transport.add_response(json!([generate_log("awesome")]));
 
-        let log = LogFilterBuilder::new(web3)
-            .stream()
-            .boxed()
-            .next()
+        let logs = LogFilterBuilder::new(web3)
+            .from_block(66.into())
+            .to_block(BlockNumber::Pending)
+            .address(vec![address])
+            .topic0(Topic::This(topics[0]))
+            .topic1(Topic::This(topics[1]))
+            .topic2(Topic::This(topics[2]))
+            .topic3(Topic::OneOf(vec![topics[3]; 3]))
+            .limit(42)
+            .block_page_size(5) // NOTE: This should get ignored.
+            .poll_interval(Duration::from_secs(100)) // NOTE: This should get ignored.
+            .past_logs()
             .immediate()
-            .expect("log stream did not produce any logs")
-            .expect("failed to get log from log stream");
+            .expect("failed to get past logs");
 
-        assert_eq!(log.log_type.as_deref(), Some("awesome"));
-        transport.assert_request("eth_newFilter", &[json!({})]);
-        transport.assert_request("eth_getFilterChanges", &[json!("0xf0")]);
+        assert_eq!(logs[0].log_type.as_deref(), Some("awesome"));
+        transport.assert_request(
+            "eth_getLogs",
+            &[json!({
+                "address": address,
+                "fromBlock": U64::from(66),
+                "toBlock": BlockNumber::Pending,
+                "topics": [
+                    topics[0],
+                    topics[1],
+                    topics[2],
+                    vec![topics[3]; 3],
+                ],
+                "limit": 42,
+            })],
+        );
         transport.assert_no_more_requests();
     }
 
@@ -419,6 +444,7 @@ mod tests {
             .to_block(BlockNumber::Pending)
             .address(vec![address])
             .topic0(Topic::This(topic))
+            .limit(42) // NOTE: This should get ignored.
             .block_page_size(5)
             .past_logs_pages()
             .boxed();
@@ -472,6 +498,30 @@ mod tests {
                 "topics": [topic],
             })],
         );
+        transport.assert_no_more_requests();
+    }
+
+    #[test]
+    fn log_stream_next_log() {
+        let mut transport = TestTransport::new();
+        let web3 = Web3::new(transport.clone());
+
+        // filter created
+        transport.add_response(json!("0xf0"));
+        // get logs filter
+        transport.add_response(json!([generate_log("awesome")]));
+
+        let log = LogFilterBuilder::new(web3)
+            .stream()
+            .boxed()
+            .next()
+            .immediate()
+            .expect("log stream did not produce any logs")
+            .expect("failed to get log from log stream");
+
+        assert_eq!(log.log_type.as_deref(), Some("awesome"));
+        transport.assert_request("eth_newFilter", &[json!({})]);
+        transport.assert_request("eth_getFilterChanges", &[json!("0xf0")]);
         transport.assert_no_more_requests();
     }
 }
