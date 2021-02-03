8158ebdfc6351f43bd75abc3a9b9535a785ec0f5
Valentin <77051586+vkgnosis@users.noreply.github.com>
2021-02-03 18:42:12 +0100

    Fix missing Some in deployment transaction hash

--- examples/generate/build.rs
+++ examples/generate/build.rs
@@ -1,10 +1,11 @@
-use ethcontract_generate::Builder;
+use ethcontract_generate::{Address, Builder, TransactionHash};
 use std::env;
 use std::path::Path;
 
 fn main() {
     let dest = env::var("OUT_DIR").unwrap();
     Builder::new("../truffle/build/contracts/RustCoin.json")
+        .add_deployment(42, Address::zero(), Some(TransactionHash::zero()))
         .generate()
         .unwrap()
         .write_to_file(Path::new(&dest).join("rust_coin.rs"))
