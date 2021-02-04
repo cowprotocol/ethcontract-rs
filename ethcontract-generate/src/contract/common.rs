004a0250fc7b45eef74ce148f2f8a635bc2dd206
Valentin <77051586+vkgnosis@users.noreply.github.com>
2021-02-04 13:11:05 +0100

    Fix missing Some in deployment transaction hash (#463)

--- ethcontract-generate/src/contract/common.rs
+++ ethcontract-generate/src/contract/common.rs
@@ -185,7 +185,7 @@ fn expand_transaction_hash(hash: Option<TransactionHash>) -> TokenStream {
 
     let bytes = hash.as_bytes().iter().copied().map(Literal::u8_unsuffixed);
     quote! {
-        self::ethcontract::TransactionHash([#( #bytes ),*])
+        Some(self::ethcontract::TransactionHash([#( #bytes ),*]))
     }
 }
 
@@ -219,7 +219,7 @@ mod tests {
         assert_quote!(
             expand_transaction_hash(Some("000102030405060708090a0b0c0d0e0f10111213000000000000000000000000".parse().unwrap())),
             {
-                self::ethcontract::TransactionHash([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+                Some(self::ethcontract::TransactionHash([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
             },
         );
     }
