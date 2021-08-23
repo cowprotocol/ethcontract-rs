2653e242f2e575ee8ca803ebb158925ffb103212
Tamika Nomara <tamika.nomara@gnosis.pm>
2021-08-23 17:43:16 +0300

    More utils (pull useful stuff from cowswap services) (#614)

--- ethcontract-mock/src/utils.rs
+++ ethcontract-mock/src/utils.rs
@@ -19,6 +19,19 @@ pub fn address_for(who: &str) -> Address {
     account_for(who).address()
 }
 
+/// Shortcut for [`address_for`]`("Alice")`.
+///
+/// # Examples
+///
+/// ```
+/// # use ethcontract_mock::utils::address;
+/// let address = address();
+/// # assert_eq!(address, "0xbf0b5a4099f0bf6c8bc4252ebec548bae95602ea".parse().unwrap());
+/// ```
+pub fn address() -> Address {
+    address_for("Alice")
+}
+
 /// Generate a private key by hashing the given string.
 ///
 /// # Safety
@@ -38,4 +51,50 @@ pub fn account_for(who: &str) -> Account {
         PrivateKey::from_raw(keccak256(who.as_bytes())).unwrap(),
         None,
     )
+}
+
+/// Shortcut for [`account_for`]`("Alice")`.
+///
+/// # Examples
+///
+/// ```
+/// # use ethcontract_mock::utils::account;
+/// let account = account();
+/// # assert_eq!(account.address(), "0xbf0b5a4099f0bf6c8bc4252ebec548bae95602ea".parse().unwrap());
+/// ```
+pub fn account() -> Account {
+    account_for("Alice")
+}
+
+/// Deploy a mocked version of a generated contract.
+///
+/// # Parameters
+///
+/// - `mock`: a [Mock] instance.
+/// - `contract` type of the contract.
+///
+/// # Examples
+///
+/// ```
+/// # use ethcontract_mock::{Mock, mock_contract};
+/// # ethcontract::contract!(
+/// #     "../examples/truffle/build/contracts/IERC20.json",
+/// #     contract = IERC20 as ERC20,
+/// # );
+/// # fn main() {
+/// let mock = Mock::new(1234);
+/// let (contract, instance) = mock_contract!(mock, ERC20);
+/// # }
+/// ```
+///
+/// [Mock]: crate::Mock
+#[macro_export]
+macro_rules! mock_contract {
+    ($mock:ident, $contract:ident) => {{
+        let mock = $mock;
+        let contract = mock.deploy($contract::raw_contract().abi.clone());
+        let instance = $contract::at(&contract.web3(), contract.address());
+
+        (contract, instance)
+    }};
 }
